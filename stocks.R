b00eeac9cd8c7233e30efe9fc80f0afafb6edbf7
Mcatapano2022 <mcatapan@vols.utk.edu>
2022-03-09 16:51:01 -0800

    Update stocks.R
    added  select date feature

--- stocks.R
+++ stocks.R
@@ -21,3 +21,20 @@ selected_stocks <- c("GOOG", "AAPL")
 stocks %>%
   filter(symbol %in% selected_stocks) %>%
   autoplot(volume)
+
+#Add dates min and max
+
+server <- function(input,output){
+  output$ts_plot <- renderPlot({
+    first.date <- input$selected_date_range[1]
+    second.date <- input$selected_date_range[2]
+    
+    plot.selctdate <- stocks[stocks$symbol == input$selected_stock,]
+    
+    plot.selctdate <- plot.selctdate[plot.selctdate$date >= min_date,]
+    plot.selctdate <- plot.selctdatef[plot.selctdate$date <= max_date,]
+    
+    autoplot(plot.selctdate, .vars = close)
+  })
+  
+}
